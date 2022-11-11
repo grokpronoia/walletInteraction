15dca296978eaeb4e8dd08a44f585ace29c36aa0
Grok Pronoia <14910375+grokpronoia@users.noreply.github.com>
2022-11-10 19:52:59 -0500

    added support for bsc and poly

--- blockConversion.py
+++ blockConversion.py
@@ -1,14 +1,17 @@
 import requests
 
-API_KEY = ''
+ETH_API_KEY = ''
+ARB_API_KEY = ''
+BSC_API_KEY = ''
+POL_API_KEY = ''
 
-def blockNumber(time, place):
+def blockNumberETH(time, place):
     bp = {
         "module": 'block',
         "action": 'getblocknobytime',
         "timestamp": time,
         "closest": place,
-        "apikey": API_KEY
+        "apikey": ETH_API_KEY
     }
 
     response = requests.get('https://api.etherscan.io/api?', params=bp)
@@ -16,3 +19,49 @@ def blockNumber(time, place):
     block = response.json()['result']
     print(block)
     return block
+
+def blockNumberARB(time, place):
+    bp = {
+        "module": 'block',
+        "action": 'getblocknobytime',
+        "timestamp": time,
+        "closest": place,
+        "apikey": ARB_API_KEY
+    }
+
+    response = requests.get('https://api.arbiscan.io/api?', params=bp)
+    response.raise_for_status()
+    block = response.json()['result']
+    print(block)
+    return block
+
+def blockNumberBSC(time, place):
+    bp = {
+        "module": 'block',
+        "action": 'getblocknobytime',
+        "timestamp": time,
+        "closest": place,
+        "apikey": BSC_API_KEY
+    }
+
+    response = requests.get('https://api.bscscan.com/api?', params=bp)
+    response.raise_for_status()
+    block = response.json()['result']
+    print(block)
+    return block
+
+def blockNumberPOL(time, place):
+    bp = {
+        "module": 'block',
+        "action": 'getblocknobytime',
+        "timestamp": time,
+        "closest": place,
+        "apikey": POL_API_KEY
+    }
+
+    response = requests.get('https://api.polygonscan.com/api?', params=bp)
+    response.raise_for_status()
+    block = response.json()['result']
+    print(block)
+    return block
+
--- walletList.py
+++ walletList.py
@@ -1,15 +1,36 @@
 import sys
 from rawConversion import dateConverter
-from blockConversion import blockNumber
-from transactionAddress import getWallet
+from blockConversion import blockNumberETH, blockNumberARB, blockNumberBSC, blockNumberPOL
+from transactionAddressETH import getWalletETH
+from transactionAddressARB import getWalletARB
+from transactionAddressBSC import getWalletBSC
+from transactionAddressPOL import getWalletPOL
+
 
 def main():
-    start = dateConverter(sys.argv[1], "before")
-    end = dateConverter(sys.argv[2], "after")
-    first = int(blockNumber(start, "before"))
-    next = first + 5000
-    last = int(blockNumber(end, "after"))
-    getWallet(sys.argv[3], first, next, last)
+    start = dateConverter(sys.argv[2], "before")
+    end = dateConverter(sys.argv[3], "after")
+    if sys.argv[1] == "eth":
+        first = int(blockNumberETH(start, "before"))
+        next = first + 5000
+        last = int(blockNumberETH(end, "after"))
+        getWalletETH(sys.argv[4], first, next, last)
+    elif sys.argv[1] == "arb":
+        first = int(blockNumberARB(start, "before"))
+        next = first + 5000
+        last = int(blockNumberARB(end, "after"))
+        getWalletARB(sys.argv[4], first, next, last)
+    elif sys.argv[1] == "poly":
+        first = int(blockNumberPOL(start, "before"))
+        next = first + 5000
+        last = int(blockNumberPOL(end, "after"))
+        getWalletPOL(sys.argv[4], first, next, last)
+    else:
+        first = int(blockNumberBSC(start, "before"))
+        next = first + 5000
+        last = int(blockNumberBSC(end, "after"))
+        getWalletBSC(sys.argv[4], first, next, last)
 
+        
 if __name__ == "__main__":
     main()
